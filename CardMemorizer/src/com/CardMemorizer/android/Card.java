d3baa99de390628cc2816ee6e0269a12a4dbded6
Jacob Applin <japplin@kobo.com>
2013-08-28 00:38:41 -0400

    Refactored the card creation and checkChanged listeners

--- CardMemorizer/src/com/CardMemorizer/android/Card.java
+++ CardMemorizer/src/com/CardMemorizer/android/Card.java
@@ -42,11 +42,11 @@ public class Card extends RelativeLayout {
 	private ImageView cardFront;
 	private ImageView cardSelected;
 
-	public Card(Context context, int cardId, Rank rank, Suit suit) {
+	public Card(Context context, Rank rank, Suit suit) {
 		super(context);
-		this.id = cardId;
 		this.rank = rank;
 		this.suit = suit;
+		this.id = getImageId(suit, rank);
 
 		cardFront = new ImageView(context);
 		cardFront.setImageResource(id);
@@ -102,6 +102,155 @@ public class Card extends RelativeLayout {
 
 	public void setSelectedState(boolean isSelected) {
 		cardSelected.setVisibility(isSelected ? View.VISIBLE : View.GONE);
+	}
+
+	private int getImageId(Suit suit, Rank rank) {
+		switch (rank) {
+		case ace:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_ace;
+			case diamonds:
+				return R.drawable.diamonds_ace;
+			case hearts:
+				return R.drawable.hearts_ace;
+			case spades:
+				return R.drawable.spades_ace;
+			}
+		case eight:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_8;
+			case diamonds:
+				return R.drawable.diamonds_8;
+			case hearts:
+				return R.drawable.hearts_8;
+			case spades:
+				return R.drawable.spades_9;
+			}
+		case five:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_5;
+			case diamonds:
+				return R.drawable.diamonds_5;
+			case hearts:
+				return R.drawable.hearts_5;
+			case spades:
+				return R.drawable.spades_5;
+			}
+		case four:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_4;
+			case diamonds:
+				return R.drawable.diamonds_4;
+			case hearts:
+				return R.drawable.hearts_4;
+			case spades:
+				return R.drawable.spades_4;
+			}
+		case jack:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_jack;
+			case diamonds:
+				return R.drawable.diamonds_jack;
+			case hearts:
+				return R.drawable.hearts_jack;
+			case spades:
+				return R.drawable.spades_jack;
+			}
+		case king:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_king;
+			case diamonds:
+				return R.drawable.diamonds_king;
+			case hearts:
+				return R.drawable.hearts_king;
+			case spades:
+				return R.drawable.spades_king;
+			}
+		case nine:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_9;
+			case diamonds:
+				return R.drawable.diamonds_9;
+			case hearts:
+				return R.drawable.hearts_9;
+			case spades:
+				return R.drawable.spades_9;
+			}
+		case queen:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_queen;
+			case diamonds:
+				return R.drawable.diamonds_queen;
+			case hearts:
+				return R.drawable.hearts_queen;
+			case spades:
+				return R.drawable.spades_queen;
+			}
+		case seven:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_7;
+			case diamonds:
+				return R.drawable.diamonds_7;
+			case hearts:
+				return R.drawable.hearts_7;
+			case spades:
+				return R.drawable.spades_7;
+			}
+		case six:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_6;
+			case diamonds:
+				return R.drawable.diamonds_6;
+			case hearts:
+				return R.drawable.hearts_6;
+			case spades:
+				return R.drawable.spades_6;
+			}
+		case ten:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_10;
+			case diamonds:
+				return R.drawable.diamonds_10;
+			case hearts:
+				return R.drawable.hearts_10;
+			case spades:
+				return R.drawable.spades_10;
+			}
+		case three:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_3;
+			case diamonds:
+				return R.drawable.diamonds_3;
+			case hearts:
+				return R.drawable.hearts_3;
+			case spades:
+				return R.drawable.spades_3;
+			}
+		case two:
+			switch (suit) {
+			case clubs:
+				return R.drawable.clubs_2;
+			case diamonds:
+				return R.drawable.diamonds_2;
+			case hearts:
+				return R.drawable.hearts_2;
+			case spades:
+				return R.drawable.spades_2;
+			}
+		}
+		return 0;
 	}
 
 }
--- CardMemorizer/src/com/CardMemorizer/android/MainActivity.java
+++ CardMemorizer/src/com/CardMemorizer/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.CardMemorizer.android;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -16,145 +19,72 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import com.CardMemorizer.android.Card.Rank;
+import com.CardMemorizer.android.Card.Suit;
 
 public class MainActivity extends Activity {
 
-	private int deckSize;
-	private boolean addClubs;
-	private boolean addSpades;
-	private boolean addDiamonds;
-	private boolean addHearts;
-	private int maxSize;
-	private ArrayList<Card> deck;
+	private LinearLayout deckContainer;
+	private DrawerLayout drawer;
+	private NumberPicker numberPicker;
+
 	private Button startButton;
 	private Button restartButton;
 	private Button selectButton;
-	private Button newgameButton;
-	private LinearLayout deckContainer;
-	private DrawerLayout drawer;
-	private CheckBox heartsChk;
-	private CheckBox diamondsChk;
-	private CheckBox clubsChk;
-	private CheckBox spadesChk;
-	private NumberPicker np;
+	private Button newGameButton;
+
+	private ArrayList<Card> deck;
+	private Set<Suit> suitsInDeck = new HashSet<Suit>(Suit.values().length);
+	private Set<Rank> ranksInDeck = new HashSet<Rank>(Rank.values().length);
+
+	private int deckSize;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		deckSize = 52;
-		addClubs = true;
-		addSpades = true;
-		addDiamonds = true;
-		addHearts = true;
-		maxSize = 52;
+		deckSize = calculateDeckSize();
 		deck = new ArrayList<Card>(deckSize);
+
 		startButton = ((Button) findViewById(R.id.start));
 		restartButton = ((Button) findViewById(R.id.restart));
 		selectButton = ((Button) findViewById(R.id.select));
-		newgameButton = ((Button) findViewById(R.id.new_game));
+		newGameButton = ((Button) findViewById(R.id.new_game));
+
 		deckContainer = ((LinearLayout) findViewById(R.id.shuffled_card_container));
 		drawer = ((DrawerLayout) findViewById(R.id.drawer_layout));
-		heartsChk = ((CheckBox) findViewById(R.id.chkhearts));
-		diamondsChk = ((CheckBox) findViewById(R.id.chkdiamonds));
-		clubsChk = ((CheckBox) findViewById(R.id.chkclubs));
-		spadesChk = ((CheckBox) findViewById(R.id.chkspades));
 
-		np = (NumberPicker) findViewById(R.id.np);
-		np.setMaxValue(52);
-		np.setMinValue(1);
+		numberPicker = (NumberPicker) findViewById(R.id.np);
+		numberPicker.setOnValueChangedListener(new OnValueChangeListener() {
 
-		heartsChk.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					addHearts = true;
-				} else {
-					addHearts = false;
-				}
-				setNPRange();
-
-				if (!addClubs && !addSpades && !addHearts && !addDiamonds) {
-					newgameButton.setEnabled(false);
-				} else {
-					newgameButton.setEnabled(true);
-				}
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+				ranksInDeck = new HashSet<Rank>(Arrays.asList(Rank.values()).subList(0, newVal));
+				deckSize = calculateDeckSize();
 			}
 		});
+		numberPicker.setMaxValue(Rank.values().length);
+		numberPicker.setMinValue(1);
 
-		diamondsChk.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					addDiamonds = true;
-				} else {
-					addDiamonds = false;
-				}
-				setNPRange();
+		((CheckBox) findViewById(R.id.chkhearts)).setOnCheckedChangeListener(createSuitCheckChanged(Suit.hearts));
+		((CheckBox) findViewById(R.id.chkdiamonds)).setOnCheckedChangeListener(createSuitCheckChanged(Suit.diamonds));
+		((CheckBox) findViewById(R.id.chkclubs)).setOnCheckedChangeListener(createSuitCheckChanged(Suit.clubs));
+		((CheckBox) findViewById(R.id.chkspades)).setOnCheckedChangeListener(createSuitCheckChanged(Suit.spades));
 
-				if (!addClubs && !addSpades && !addHearts && !addDiamonds) {
-					newgameButton.setEnabled(false);
-				} else {
-					newgameButton.setEnabled(true);
-				}
-			}
-		});
-
-		clubsChk.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					addClubs = true;
-				} else {
-					addClubs = false;
-				}
-				setNPRange();
-
-				if (!addClubs && !addSpades && !addHearts && !addDiamonds) {
-					newgameButton.setEnabled(false);
-				} else {
-					newgameButton.setEnabled(true);
-				}
-			}
-		});
-
-		spadesChk.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				if (isChecked) {
-					addSpades = true;
-				} else {
-					addSpades = false;
-				}
-				setNPRange();
-
-				if (!addClubs && !addSpades && !addHearts && !addDiamonds) {
-					newgameButton.setEnabled(false);
-				} else {
-					newgameButton.setEnabled(true);
-				}
-			}
-		});
-
-		newgameButton.setOnClickListener(new OnClickListener() {
+		newGameButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				deck = createDeck(addClubs, addSpades, addDiamonds, addHearts);
+				deck = createDeck();
 				Collections.shuffle(deck);
 				deckContainer.removeAllViews();
 
-				int dcount2 = 0;
-
-				for (Card card : deck) {
-					if (dcount2 < np.getValue()) {
-						card.setSelectedState(false);
-						card.shouldShowBack(false);
-						deckContainer.addView(card);
-					}
-					dcount2 = dcount2 + 1;
-				}
+				addDeckToLayout(deck);
 
 				CardMemorizerSavedState.getInstance().setIsRunning(false);
 				updateButtonVisibility();
@@ -165,12 +95,33 @@ public class MainActivity extends Activity {
 		if (CardMemorizerSavedState.getInstance().isRunning()) {
 			deck = new ArrayList<Card>(CardMemorizerSavedState.getInstance().getShuffledDeck());
 		} else {
-			deck = createDeck(addClubs, addSpades, addDiamonds, addHearts);
+			deck = createDeck();
 			Collections.shuffle(deck);
 		}
 
-		addDeckToLayout(deck, R.id.shuffled_card_container, maxSize);
+		addDeckToLayout(deck);
 		setupButtons();
+	}
+
+	private int calculateDeckSize() {
+		return ranksInDeck.size() * suitsInDeck.size();
+	}
+
+	private OnCheckedChangeListener createSuitCheckChanged(final Suit suit) {
+		return new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					suitsInDeck.add(suit);
+				} else {
+					suitsInDeck.remove(suit);
+				}
+
+				deckSize = calculateDeckSize();
+			}
+		};
+
 	}
 
 	public void setupButtons() {
@@ -251,112 +202,27 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	private ArrayList<Card> createDeck(boolean addClubs, boolean addSpades, boolean addDiamonds, boolean addHearts) {
+	private ArrayList<Card> createDeck() {
 		ArrayList<Card> deck = new ArrayList<Card>(deckSize);
 
-		if (addClubs) {
-			deck.add(new Card(this, R.drawable.clubs_ace, Card.Rank.ace, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_2, Card.Rank.two, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_3, Card.Rank.three, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_4, Card.Rank.four, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_5, Card.Rank.five, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_6, Card.Rank.six, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_7, Card.Rank.seven, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_8, Card.Rank.eight, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_9, Card.Rank.nine, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_10, Card.Rank.ten, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_jack, Card.Rank.jack, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_queen, Card.Rank.queen, Card.Suit.clubs));
-			deck.add(new Card(this, R.drawable.clubs_king, Card.Rank.king, Card.Suit.clubs));
-		}
-
-		if (addDiamonds) {
-			deck.add(new Card(this, R.drawable.diamonds_ace, Card.Rank.ace, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_2, Card.Rank.two, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_3, Card.Rank.three, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_4, Card.Rank.four, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_5, Card.Rank.five, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_6, Card.Rank.six, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_7, Card.Rank.seven, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_8, Card.Rank.eight, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_9, Card.Rank.nine, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_10, Card.Rank.ten, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_jack, Card.Rank.jack, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_queen, Card.Rank.queen, Card.Suit.diamonds));
-			deck.add(new Card(this, R.drawable.diamonds_king, Card.Rank.king, Card.Suit.diamonds));
-		}
-
-		if (addHearts) {
-			deck.add(new Card(this, R.drawable.hearts_ace, Card.Rank.ace, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_2, Card.Rank.two, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_3, Card.Rank.three, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_4, Card.Rank.four, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_5, Card.Rank.five, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_6, Card.Rank.six, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_7, Card.Rank.seven, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_8, Card.Rank.eight, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_9, Card.Rank.nine, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_10, Card.Rank.ten, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_jack, Card.Rank.jack, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_queen, Card.Rank.queen, Card.Suit.hearts));
-			deck.add(new Card(this, R.drawable.hearts_king, Card.Rank.king, Card.Suit.hearts));
-		}
-
-		if (addSpades) {
-			deck.add(new Card(this, R.drawable.spades_ace, Card.Rank.ace, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_2, Card.Rank.two, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_3, Card.Rank.three, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_4, Card.Rank.four, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_5, Card.Rank.five, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_6, Card.Rank.six, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_7, Card.Rank.seven, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_8, Card.Rank.eight, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_9, Card.Rank.nine, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_10, Card.Rank.ten, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_jack, Card.Rank.jack, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_queen, Card.Rank.queen, Card.Suit.spades));
-			deck.add(new Card(this, R.drawable.spades_king, Card.Rank.king, Card.Suit.spades));
+		for (Rank rank : ranksInDeck) {
+			for (Suit suit : suitsInDeck) {
+				deck.add(new Card(this, rank, suit));
+			}
 		}
 		return deck;
 	}
 
-	public void addDeckToLayout(ArrayList<Card> deck, int id, int max) {
+	public void addDeckToLayout(ArrayList<Card> deck) {
+		deckContainer.removeAllViews();
 
-		int dcount = 0;
-
-		for (Card s : deck) {
-			if (dcount < max) {
-				deckContainer.addView(s);
-			}
-			dcount = dcount + 1;
+		for (int i = 0; i < deckSize; i++) {
+			deckContainer.addView(deck.get(i));
 		}
 	}
 
 	public void onRadioButtonClicked(View view) {
 		((RadioButton) view).isChecked();
-
 	}
 
-	public void setNPRange() {
-		int range = 0;
-
-		if (addClubs) {
-			range += Rank.values().length;
-		}
-		if (addDiamonds) {
-			range += Rank.values().length;
-			;
-		}
-		if (addSpades) {
-			range += Rank.values().length;
-			;
-		}
-		if (addHearts) {
-			range += Rank.values().length;
-			;
-		}
-
-		np.setMaxValue(range);
-		np.setMinValue(1);
-	}
 }
