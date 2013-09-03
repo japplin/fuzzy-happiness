8d76d28bc688b066254a9946bd98b372f5387e9a
Jacob Applin <japplin@kobo.com>
2013-09-03 13:45:34 -0400

    Created level class

---
+++ CardMemorizer/src/com/CardMemorizer/android/Level.java
@@ -0,0 +1,34 @@
+package com.CardMemorizer.android;
+
+import java.util.Set;
+
+import com.CardMemorizer.android.Card.Rank;
+import com.CardMemorizer.android.Card.Suit;
+
+public class Level {
+
+	private Set<Suit> suits;
+
+	private Set<Rank> ranks;
+
+	private int deckSize;
+
+	public Level(Set<Suit> suits, Set<Rank> ranks, int deckSize) {
+		this.suits = suits;
+		this.ranks = ranks;
+		this.deckSize = deckSize;
+	}
+
+	public Set<Suit> getSuits() {
+		return suits;
+	}
+
+	public Set<Rank> getRanks() {
+		return ranks;
+	}
+
+	public int getDeckSoze() {
+		return deckSize;
+	}
+
+}
