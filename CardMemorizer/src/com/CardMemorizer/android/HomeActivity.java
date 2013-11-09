af51a5578617ff260aadfd13103de201887d63eb
Adam Wisniewski <adam.wisniewski@live.com>
2013-11-09 15:55:43 -0500

    homescreen layout improvements

--- CardMemorizer/src/com/CardMemorizer/android/HomeActivity.java
+++ CardMemorizer/src/com/CardMemorizer/android/HomeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends Activity {
 
@@ -13,6 +14,28 @@ public class HomeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_page);
+
+		ImageView hearticon;
+		ImageView spadeicon;
+		ImageView diamondicon;
+		ImageView clubicon;
+		ImageView altlogo;
+
+		hearticon = (ImageView) findViewById(R.id.heart);
+		hearticon.setImageResource(R.drawable.hearts);
+
+		spadeicon = (ImageView) findViewById(R.id.spade);
+		spadeicon.setImageResource(R.drawable.spades);
+
+		diamondicon = (ImageView) findViewById(R.id.diamond);
+		diamondicon.setImageResource(R.drawable.diamonds);
+
+		clubicon = (ImageView) findViewById(R.id.club);
+		clubicon.setImageResource(R.drawable.clubs);
+
+		altlogo = (ImageView) findViewById(R.id.altlogo);
+		altlogo.setImageResource(R.drawable.altlogo);
+
 		((Button) findViewById(R.id.level_mode)).setOnClickListener(new OnClickListener() {
 
 			@Override
