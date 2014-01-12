2e123bd451f87b089e830c433696287c6a6c910d
Jacob Applin <japplin@kobo.com>
2014-01-12 15:44:44 -0500

    Changed Navigation mode to tabs for the home activity

--- CardMemorizer/src/com/CardMemorizer/android/MainNavigation/HomeActivity.java
+++ CardMemorizer/src/com/CardMemorizer/android/MainNavigation/HomeActivity.java
@@ -1,5 +1,8 @@
 package com.CardMemorizer.android.MainNavigation;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -16,10 +19,48 @@ public class HomeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_page);
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
 
+			@Override
+			public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			}
+
+			@Override
+			public void onTabSelected(Tab tab, FragmentTransaction ft) {
+				mViewPager.setCurrentItem(tab.getPosition());
+			}
+
+			@Override
+			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			}
+		};
+		
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mAdapter = new MainNavFragmentPagerAdapter(getSupportFragmentManager());
 		mViewPager.setAdapter(mAdapter);
-	}
+		mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
+			@Override
+			public void onPageSelected(int position) {
+				getActionBar().setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		
+		actionBar.addTab(actionBar.newTab().setText(R.string.levels).setTabListener(tabListener));
+		actionBar.addTab(actionBar.newTab().setText(R.string.custom_game).setTabListener(tabListener));
+	}
 }
